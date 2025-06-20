994a266319f09f7d7df43e438ea50a587664f81b
Bastian Andres Palavecino Ancalaf <ba.palavecino@duocuc.cl>
2025-06-20 01:34:33 -0400

    Models.py
    se agregaron nuevos datos para el registro de los veterinarios, con la opción de agregar su institución mediante un menú desplegable de opciones.

--- MascotApp/veterinarias/models.py
+++ MascotApp/veterinarias/models.py
@@ -1,13 +1,34 @@
 from django.db import models
 
 # Create your models here.
+instituciones = [
+    ('UChile', 'Universidad de Chile'),
+    ('PUC', 'Pontificia Universidad Católica de Chile'),
+    ('UCMaule', 'Universidad Católica del Maule'),
+    ('UAustral', 'Universidad Austral de Chile'),
+    ('UdeC', 'Universidad de Concepción'),
+    ('UMayor', 'Universidad Mayor'),
+    ('USS', 'Universidad San Sebastián'),
+    ('UDLA', 'Universidad de Las Américas'),
+    ('UBO', 'Universidad Bernardo O’Higgins'),
+    ('UCTemuco', 'Universidad Católica de Temuco'),
+    ('USTomas', 'Universidad Santo Tomás'),
+    ('UVM', 'Universidad Viña del Mar'),
+    ('UNAB', 'Universidad Nacional Andrés Bello'),
+    ('DuocUC', 'Duoc UC'),
+    ('IPValleCentral', 'Instituto Profesional Valle Central'),
+    ('IDMA', 'Centro de Formación Técnica IDMA'),
+    ('UAconcagua', 'Universidad de Aconcagua'),
+]
 
-class veterinarios(models.Model):
-    Nombre = models.CharField(max_length=200)
-    Apellido = models.CharField(max_length=200)
-    Email = models.EmailField()
-    Certificado = models.FileField(upload_to='certificados/')
-    verificado = models.BooleanField(default=False) 
+class Veterinario(models.Model):
+    nombre = models.CharField(max_length=200)
+    apellido = models.CharField(max_length=200)
+    rut = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    certificado = models.FileField(upload_to='certificados/')
+    institucion = models.CharField(max_length=100, choices=instituciones)
+    verificado = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.Nombre
+        return f"{self.nombre} {self.apellido}"
